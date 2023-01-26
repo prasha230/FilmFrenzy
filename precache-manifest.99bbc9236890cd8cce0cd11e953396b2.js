46e17b64e846cc2d7db1290db884422a4b7c77ee
Prashant Rai <prashantrai230@gmail.com>
2023-01-26 21:42:57 +0530

    Updates

---
+++ precache-manifest.99bbc9236890cd8cce0cd11e953396b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "570b878feb46fc20dae4",
+    "url": "/FilmFrenzy/static/css/main.84821259.chunk.css"
+  },
+  {
+    "revision": "570b878feb46fc20dae4",
+    "url": "/FilmFrenzy/static/js/main.570b878f.chunk.js"
+  },
+  {
+    "revision": "205849d973eaa941ab0f",
+    "url": "/FilmFrenzy/static/js/1.205849d9.chunk.js"
+  },
+  {
+    "revision": "5b364325c048e5c1c688",
+    "url": "/FilmFrenzy/static/js/2.5b364325.chunk.js"
+  },
+  {
+    "revision": "cd40da245e007be6c13a",
+    "url": "/FilmFrenzy/static/js/runtime~main.cd40da24.js"
+  },
+  {
+    "revision": "9e6130f069f6214b55ba84fa616bd09d",
+    "url": "/FilmFrenzy/static/media/background.9e6130f0.jpg"
+  },
+  {
+    "revision": "ff922436391d572132c1b0c1c7ec9d5b",
+    "url": "/FilmFrenzy/index.html"
+  }
+];
